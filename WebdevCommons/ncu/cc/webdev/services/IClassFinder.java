412f4fc5eed2b3f889805ffd05f4554dd46e2e7f
linuzilla <linuzilla@gmail.com>
2012-03-29 11:39:33 +0800

    .

--- WebdevCommons/ncu/cc/webdev/services/IClassFinder.java
+++
@@ -1,8 +0,0 @@
-package ncu.cc.webdev.services;
-
-import java.io.IOException;
-
-public interface IClassFinder {
-	public Class<?>[]	findClassesByPackage(String packageName) throws IOException;
-	public boolean		isa(Class<?> myclazz, Class<?> clazz);
-}
